768f4b19c8fafbc8ef30db70ad00ac64d282e9d7
Pranay <pmcanvas4501@gmail.com>
2022-10-19 16:22:53 +0530

    prime number and palindrome solution

---
+++ Practice2.js
@@ -0,0 +1,13 @@
+let num = 1221;
+
+let str=num.toString();
+
+let bag="";
+
+for(let i=str.length-1;i>=0;i++){
+    bag+=str[i] ; 
+}if(bag==str){
+    console.log(num,"is a palindrome")
+}else{
+    console.log(num,"is not a palindrome")
+}
