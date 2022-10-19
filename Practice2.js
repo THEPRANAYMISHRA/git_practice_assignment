2a8940d4ed56c7d589aa2329bd9afc2258b1b9d5
Akshay <akshaykumargaurav@gmail.com>
2022-10-19 18:10:07 +0530

    fix_the_given_code

--- Practice2.js
+++ Practice2.js
@@ -4,7 +4,7 @@ let str=num.toString();
 
 let bag="";
 
-for(let i=str.length-1;i>=0;i++){
+for(let i=str.length-1;i>=0;i--){
     bag+=str[i] ; 
 }if(bag==str){
     console.log(num,"is a palindrome")
